d437a03221549fc34aa63dcfee3fc9b5a7a2b911
whes1015 <44525760+whes1015@users.noreply.github.com>
2021-11-30 22:23:23 +0800

    Add files via upload

--- cam.py
+++ cam.py
@@ -34,28 +34,24 @@ def on_info(server: PluginServerInterface, info: Info):
     if info.is_player and info.content == '!!cam':
         name = info.player
         if server.rcon_query('data get entity {} playerGameType'.format(name)).find('3') == -1:
-            position = process_coordinate(re.search(
-                r'\[.*]', server.rcon_query('data get entity {} Pos'.format(name))).group())
+            position = process_coordinate(re.search( r'\[.*]', server.rcon_query('data get entity {} Pos'.format(name))).group())
             Json[name+'X'] = position.x
             Json[name+'Y'] = position.y
             Json[name+'Z'] = position.z
-            Json[name+'RotX'] = server.rcon_query('data get entity {} Rotation[0]'.format(
-                name)).replace("{} has the following entity data: ".format(name), "").replace('f', "")
-            Json[name+'RotY'] = server.rcon_query('data get entity {} Rotation[1]'.format(
-                name)).replace("{} has the following entity data: ".format(name), "").replace('f', "")
+            #Json[name+'RotX'] = server.rcon_query('data get entity {} Rotation[0]'.format( name)).replace("{} has the following entity data: ".format(name), "").replace('f', "")
+            #Json[name+'RotY'] = server.rcon_query('data get entity {} Rotation[1]'.format(name)).replace("{} has the following entity data: ".format(name), "").replace('f', "")
             if server.rcon_query('data get entity {} Dimension'.format(name)).find('minecraft:the_nether') != -1:
-                Json[name+'Dimension'] = 'minecraft:the_nether'
+                Json[name+'D'] = 'minecraft:the_nether'
             elif server.rcon_query('data get entity {} Dimension'.format(name)).find('minecraft:the_end') != -1:
-                Json[name+'Dimension'] = 'minecraft:the_end'
+                Json[name+'D'] = 'minecraft:the_end'
             elif server.rcon_query('data get entity {} Dimension'.format(name)).find('minecraft:overworld') != -1:
-                Json[name+'Dimension'] = 'minecraft:overworld'
+                Json[name+'D'] = 'minecraft:overworld'
             f = open('C:/Users/whes1/Desktop/server/plugins/playerdata.json', 'w')
             f.write(json.dumps(Json))
             f.close()
             server.execute('gamemode spectator {}'.format(name))
         else:
-            server.execute('execute as {} in {} run teleport {} {} {}'.format(
-                name, Json[name+'Dimension'], Json[name+'X'], Json[name+'Y'], Json[name+'Z']))
+            server.execute('execute as {} in {} run teleport {} {} {}'.format( name, Json[name+'D'], Json[name+'X'], Json[name+'Y'], Json[name+'Z']))
             server.execute('gamemode survival {}'.format(name))
 
 
